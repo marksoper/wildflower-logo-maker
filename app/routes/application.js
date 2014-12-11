38fa2faf766fbc19122c38f577a10b8075e46c21
Brian Whitton <me@brianwhitton.com>
2014-12-11 16:38:16 -0500

    initial app styles. starting to integrate in-memory data

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,33 @@
 import Ember from "ember";
 
+var flowers = []
+
+for (var i = 1; i < 9; i++) {
+    flowers[i-1] = {
+        location : '/assets/img/flower' + i + '.jpeg'
+    }
+}
+
 export default Ember.Route.extend({
+    model: function() {
+        return {
+            flowers: flowers,
+            fonts: [{
+                title: "Wild Child",
+                id: 1
+                },{
+                title: "Helvetica",
+                id: 2
+            }],
+            palettes : [{
+                colors: ["#9D5F8F", "#74B48C"]
+            }, {
+                colors: ["#88C8C0","#6CD476"]
+            }, {
+                colors: ["#6BA056","#7DC5FD","#019897","#7DFCAC","#4AAF17","#E2F0AF"]
+            }, {
+                colors: ["#EBD0D1","#FD46CD","#F4417F","#C5BBF8","#B88A76","#ADB2E0"]
+            }]
+        }
+    }
 })
