5992534dedb565b27843c894399c415f69504d02
Brian Whitton <me@brianwhitton.com>
2014-12-11 07:38:11 -0500

    messing around with routes and template files

--- app/routes/logo.js
+++ app/routes/logo.js
@@ -1,7 +1,3 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
-    model : function(params) {
-        return this.store.find('logo', params.logo_id)
-    }
-})
+export default Ember.Route.extend({})
