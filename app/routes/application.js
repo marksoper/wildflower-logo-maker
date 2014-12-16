9cd13cce9a0ea539479975d55d3cbeef87526996
Brian Whitton <me@brianwhitton.com>
2014-12-16 09:15:40 -0500

    adds props to global models

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,37 +1,58 @@
 import Ember from "ember";
 
 export default Ember.Route.extend({
-    setupController: function(controller) {
-        controller.set('model', {
+    model: function() {
+        return {
             flowers : (function() {
                 var f = []
                 for (var i = 1; i < 9; i++) {
                     f.push( Ember.Object.create({
                         location: '/assets/img/flower' + i + '.jpeg',
-                        selected: false
+                        selected: false,
+                        id: i
                     }) )
                 }
                 return f
             })(),
-            logo : Ember.Object.create({
-                //selectedFlowers: controller.get('selectedFlowers')
-            }),
             fonts: [{
                 title: "Wild Child",
+                selected: false,
                 id: 1
                 },{
                 title: "Helvetica",
+                selected: false,
                 id: 2
             }],
-            palettes : [{
+            palettes : [Ember.Object.create({
+                id: 1,
+                selected: false,
                 colors: ["#9D5F8F", "#74B48C"]
-            }, {
+            }), Ember.Object.create({
+                id: 2,
+                selected: false,
                 colors: ["#88C8C0","#6CD476"]
-            }, {
+            }), Ember.Object.create({
+                id: 3,
+                selected: false,
                 colors: ["#6BA056","#7DC5FD","#019897","#7DFCAC","#4AAF17","#E2F0AF"]
-            }, {
+            }), Ember.Object.create({
+                id: 4,
+                selected: false,
                 colors: ["#EBD0D1","#FD46CD","#F4417F","#C5BBF8","#B88A76","#ADB2E0"]
-            }]
-        })
+            })],
+            arrangements: [Ember.Object.create({
+                id: 1,
+                name: 'top'
+            }), Ember.Object.create({
+                id: 2,
+                name: 'bottom'
+            }), Ember.Object.create({
+                id: 3,
+                name: 'left'
+            }), Ember.Object.create({
+                id: 4,
+                name: 'right'
+            })]
+        }
     }
 })
