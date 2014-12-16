92716a442891037b3cad27bc60a1239545f6ecb6
Brian Whitton <me@brianwhitton.com>
2014-12-16 09:14:39 -0500

    adds index path to "new" route

--- app/router.js
+++ app/router.js
@@ -6,7 +6,7 @@ var Router = Ember.Router.extend({
 })
 
 Router.map(function() {
-    this.route('new')
+    this.route('new', {path: '/'})
     this.route('view', {path: 'view/:logo_id'})
     this.route('logo', {path: '/:logo_id'})
 })
