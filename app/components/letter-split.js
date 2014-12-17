116547bcf005146b28c1d4aa49608c9c63729e77
Brian Whitton <me@brianwhitton.com>
2014-12-17 18:46:26 -0500

    initial text sizing algorithm

--- app/components/letter-split.js
+++ app/components/letter-split.js
@@ -1,7 +1,35 @@
 import Ember from 'ember';
 
+function linearMap(low1, hi1, point1, low2, hi2) {
+    var point2
+
+    point2 = (low2*point1 - low1*low2 + hi1*hi2 - hi2*point1) / ( hi1 - low1 )
+    return point2
+}
+
 export default Ember.Component.extend({
     classNamesBindings: ['class'],
+    attributeBindings: ['style'],
+    style: function() {
+        if ( this.get('text').match(/montessori/i) || this.get('size') === 'small' ) {
+            return ''
+        }
+
+        var letterCount = this.get('text').length,
+            sizeRange = [48, 123],
+            countRange = [5, 13],
+            size
+
+        if ( letterCount <= 5 ) {
+            size = Math.max.apply(null, sizeRange)
+        } else if ( letterCount >= 13 ) {
+            size = Math.min.apply(null, sizeRange)
+        } else {
+            size = linearMap(countRange[0], countRange[1], letterCount, sizeRange[0], sizeRange[1])
+        }
+
+        return `font-size: ${size}px;`
+    }.property('text'),
     letters: function() {
         var letters = this.get('text').split(''),
             colors = this.get('palette').colors,
