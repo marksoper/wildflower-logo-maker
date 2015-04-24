d9db2159f048164d06e1f58ed2c7d8fbef5d1695
Edward Faulkner <ef@alum.mit.edu>
2015-04-24 13:16:52 -0400

    update to latest ember-cli

--- app/app.js
+++ app/app.js
@@ -3,15 +3,16 @@ import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
 
-Ember.MODEL_FACTORY_INJECTIONS = true
+var App;
 
-var App = Ember.Application.extend({
-    modulePrefix: config.modulePrefix,
-    podModulePrefix: config.podModulePrefix,
-    Resolver: Resolver,
-    LOG_TRANSITIONS: true
-})
+Ember.MODEL_FACTORY_INJECTIONS = true;
 
-loadInitializers(App, config.modulePrefix)
+App = Ember.Application.extend({
+  modulePrefix: config.modulePrefix,
+  podModulePrefix: config.podModulePrefix,
+  Resolver: Resolver
+});
 
-export default App
+loadInitializers(App, config.modulePrefix);
+
+export default App;
