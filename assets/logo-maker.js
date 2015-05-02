e72345b5c11a224bde913d4f132bf1b3639e6ecf
Mark Soper <masoper@gmail.com>
2015-05-01 23:49:40 -0400

    remove fixtures

--- assets/logo-maker.js
+++ assets/logo-maker.js
@@ -544,22 +544,6 @@ define('logo-maker/models/jurisdiction', ['exports', 'ember-data'], function (ex
     name: DS['default'].attr('string')
   });
 
-  var createJurisdictionFixtures = function createJurisdictionFixtures() {
-    var usaStates = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District Of Columbia', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
-    var jurisdictions = [];
-    usaStates.forEach(function (stateName) {
-      jurisdictions.push({
-        id: stateName.toLowerCase().replace(/\s/g, '-'),
-        name: stateName
-      });
-    });
-    return jurisdictions;
-  };
-
-  Jurisdiction.reopenClass({
-    FIXTURES: createJurisdictionFixtures()
-  });
-
   exports['default'] = Jurisdiction;
 
 });
