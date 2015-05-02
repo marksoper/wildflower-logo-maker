02b382c4a83903e8906ae29df8de8340470b088e
Mark Soper <masoper@gmail.com>
2015-05-01 23:48:48 -0400

    removing reopenClass fixtures

--- app/models/jurisdiction.js
+++ app/models/jurisdiction.js
@@ -4,22 +4,6 @@ var Jurisdiction = DS.Model.extend({
   name: DS.attr('string')
 });
 
-var createJurisdictionFixtures = function() {
-  var usaStates = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "District Of Columbia", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"];
-  var jurisdictions = [];
-  usaStates.forEach(function(stateName) {
-    jurisdictions.push({
-      id: stateName.toLowerCase().replace(/\s/g, "-"),
-      name: stateName
-    });
-  });
-  return jurisdictions;
-};
-
-Jurisdiction.reopenClass({
-  FIXTURES: createJurisdictionFixtures()
-});
-
 export default Jurisdiction;
 
 
