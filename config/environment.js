951134958c215e040c38df427ca65c01584dff86
Brian Whitton <me@brianwhitton.com>
2015-05-03 01:25:57 -0400

    adds dev baseURL

--- config/environment.js
+++ config/environment.js
@@ -29,6 +29,7 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV.baseURL = '/wildflower-logo-maker/'
   }
 
   if (environment === 'test') {
