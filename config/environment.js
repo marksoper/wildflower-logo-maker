75a909824dadf0c2994a4fe1629582e55945dbcd
Brian Whitton <me@brianwhitton.com>
2015-05-03 01:35:55 -0400

    adds github build env

--- config/environment.js
+++ config/environment.js
@@ -29,7 +29,16 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     ENV.APP.LOG_VIEW_LOOKUPS = true;
+  }
+
+  if (environment === 'github') {
+    // ENV.APP.LOG_RESOLVER = true;
+    ENV.APP.LOG_ACTIVE_GENERATION = true;
+    // ENV.APP.LOG_TRANSITIONS = true;
+    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    ENV.APP.LOG_VIEW_LOOKUPS = true;
     ENV.baseURL = '/wildflower-logo-maker/'
+    ENV.modulePrefix = '/wildflower-logo-maker/'
   }
 
   if (environment === 'test') {
