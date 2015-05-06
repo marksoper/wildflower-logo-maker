7e184d052552b24083625ee5fdc9af9c12358268
Mark Soper <masoper@gmail.com>
2015-05-05 21:35:39 -0400

    remove fixture line in app

--- app/app.js
+++ app/app.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
-import DS from 'ember-data';
 
 var App;
 
@@ -14,11 +13,6 @@ App = Ember.Application.extend({
   Resolver: Resolver,
   LOG_TRANSITIONS: true
 });
-
-//
-// TODO: remove this for production
-//
-//App.ApplicationAdapter = DS.FixtureAdapter;
 
 loadInitializers(App, config.modulePrefix);
 
